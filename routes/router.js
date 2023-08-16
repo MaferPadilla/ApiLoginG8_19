583d3cc45f1e12ffae321ee3dad5e6c0271ad265
MaferPadilla <fernanda.padilla@unah.hn>
2023-08-16 11:00:00 -0600

    Primer Commit del Proyecto, configuración para la conexión a la Base de Datos

---
+++ routes/router.js
@@ -0,0 +1,9 @@
+"use strict";
+var  UsuarioController = require("../controllers/usuario-controller"),
+  express = require("express"),
+  router = express.Router();
+
+router
+  //RURAS USUARIO
+
+module.exports = router;
